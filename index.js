2c577125c245d598926e28b79adac8c085e7cb7b
anderutan <52438782+anderutan@users.noreply.github.com>
2023-12-21 12:04:46 +0800

    add slider to set password range

--- index.js
+++ index.js
@@ -6,19 +6,67 @@ let themeBtn = document.querySelector('.theme-btn');
 let section = document.querySelector('.section');
 let title = document.querySelector('.title');
 let description = document.querySelector('.description');
+let pwdBtn = document.querySelector('.pwd-btn');
+let pwd1 = document.querySelector('.pwd1');
+let pwd2 = document.querySelector('.pwd2');
+let slider = document.querySelector('.slider');
+let output = document.querySelector('.pwd-range');
+
+// set and show password range
+output.textContent = `Pwd Range: ${slider.value}`;
+slider.oninput = function () {
+  output.textContent = `Pwd Range: ${slider.value}`;
+};
+
+// Toggle white and dark theme
+function darkTheme() {
+  themeBtn.textContent = 'Dark Theme';
+  themeBtn.classList.add('dark');
+  section.classList.add('white');
+  title.classList.add('white');
+  description.classList.add('white');
+}
+
+function whiteTheme() {
+  themeBtn.textContent = 'White Theme';
+  themeBtn.classList.remove('dark');
+  section.classList.remove('white');
+  title.classList.remove('white');
+  description.classList.remove('white');
+}
 
 themeBtn.addEventListener('click', () => {
   if (themeBtn.textContent === 'White Theme') {
-    themeBtn.textContent = 'Dark Theme';
-    themeBtn.classList.add('dark');
-    section.classList.add('white');
-    title.classList.add('white');
-    description.classList.add('white');
+    darkTheme();
   } else {
-    themeBtn.textContent = 'White Theme';
-    themeBtn.classList.remove('dark');
-    section.classList.remove('white');
-    title.classList.remove('white');
-    description.classList.remove('white');
+    whiteTheme();
   }
+});
+
+// generate and display password
+function generateRandomPwd() {
+  let pwd = '';
+  for (let i = 0; i < slider.value; i++) {
+    let char = Math.floor(Math.random() * characters.length);
+    pwd += characters[char];
+  }
+  return pwd;
+}
+
+pwdBtn.addEventListener('click', () => {
+  pwd1.textContent = generateRandomPwd();
+  pwd1.style.cursor = 'pointer';
+  pwd2.textContent = generateRandomPwd();
+  pwd2.style.cursor = 'pointer';
+});
+
+// Copy password when click
+pwd1.addEventListener('click', () => {
+  navigator.clipboard.writeText(pwd1.textContent);
+  alert('Password one has been copied to clipboard.');
+});
+
+pwd2.addEventListener('click', () => {
+  navigator.clipboard.writeText(pwd2.textContent);
+  alert('Password two has been copied to clipboard.');
 });
